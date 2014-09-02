d9a8f81ca265799b39ec0bc364a43ce8385b49de
pedro <pedro@habilmind.com>
2014-09-03 01:54:24 +0200

    añadida carpeta www para PhoneGap

---
+++ www/jqm-search.php
@@ -0,0 +1,45 @@
+	<!-- TODO: This should become an external panel so we can add input to markup (unique ID) -->
+    <div data-role="panel" class="jqm-search-panel" data-position="right" data-display="overlay" data-theme="a">
+		<div class="jqm-search">
+			<ul class="jqm-list" data-filter-placeholder="Search demos..." data-filter-reveal="true">
+<!-- /jqm-contents.php -->
+
+    <li data-filtertext="demos homepage" data-icon="home"><a href=".././">Inicio</a></li>
+    <li data-filtertext="introducción a esta guía de aplicación"><a href="../habilmind-informacion/" data-ajax="false">Información</a></li>
+    <li data-filtertext="Si necesita soporte por alguna incidencia, por favor, use esta opción"><a href="../habilmind-incidencias/" data-ajax="false">¿Incidencias?</a></li>
+    <li data-role="collapsible" data-enhanced="true" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false" class="ui-collapsible ui-collapsible-themed-content ui-collapsible-collapsed">
+        <h3 class="ui-collapsible-heading ui-collapsible-heading-collapsed">
+            <a href="#" class="ui-collapsible-heading-toggle ui-btn ui-btn-icon-right ui-icon-carat-d">
+                Servicios<span class="ui-collapsible-heading-status"> pulsa para desplegarlos</span>
+            </a>
+        </h3>
+        <div class="ui-collapsible-content ui-body-inherit ui-collapsible-content-collapsed" aria-hidden="true">
+            <ul>
+                <li data-filtertext="Habilidades 4º primaria "><a href="../servicios-habilidadesgeneral/" data-ajax="false">Habilidades (desde 4º Prim.)</a></li>
+                <li data-filtertext="Habilidades 3º primaria" ><a href="../servicios-habilidadesinfantil/" data-ajax="false">Habilidades (hasta 3º Prim.)</a></li>
+            </ul>
+        </div>
+    </li>
+
+
+    <li data-role="collapsible" data-enhanced="true" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false" class="ui-collapsible ui-collapsible-themed-content ui-collapsible-collapsed">
+        <h3 class="ui-collapsible-heading ui-collapsible-heading-collapsed">
+            <a href="#" class="ui-collapsible-heading-toggle ui-btn ui-btn-icon-right ui-icon-carat-d">
+                Algunos ejemplos (jQuery Mobile)<span class="ui-collapsible-heading-status"> click to expand contents</span>
+            </a>
+        </h3>
+        <div class="ui-collapsible-content ui-body-inherit ui-collapsible-content-collapsed" aria-hidden="true">
+            <ul>
+                <li data-filtertext="button icons svg disc alt custom icon position"><a href="../_ejemplos-icons/" data-ajax="false">Icons</a></li>
+                <li data-filtertext=""><a href="../_ejemplos-icons-grunticon/" data-ajax="false">Grunticon loader</a></li>
+                <li data-filtertext="default theme swatches theming style css"><a href="../_ejemplos-theme-default/" data-ajax="false">Default theme</a></li>
+                <li data-filtertext="classic theme old theme swatches theming style css"><a href="../_ejemplos-theme-classic/" data-ajax="false">Classic theme</a></li>
+
+            </ul>
+        </div>
+    </li>
+
+<!-- /jqm-contents.php -->
+			</ul>
+		</div>
+	</div><!-- /panel -->
